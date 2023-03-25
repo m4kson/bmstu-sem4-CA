5af8de81be19f5bbef21136d6aa8620f4c9197ff
m4kson <limos.00@mail.ru>
2023-03-25 17:14:48 +0300

    day 1 finished

--- lab_2/spline.py
+++ lab_2/spline.py
@@ -22,3 +22,106 @@ def print_pointTable(table):
 
     print("└───────┴────────────┴────────────┘")
 
+def spline(table, x, start, end):
+    xValues = [i.getX() for i in table]
+    yValues = [i.getY() for i in table]
+
+    coeffs = calculateCoefs(xValues, yValues, start, end)
+
+    index = finedIndex(xValues, x)
+
+    y = countPolynom(x, xValues, index, coeffs)
+
+    return y
+
+def calculateCoefs(xValues, yValues, start, end):
+    aValues = findAVAlues(yValues)
+    cValues = findCValues(xValues, yValues, start, end)
+    bValues = findBValues(xValues, yValues, cValues)
+    dValues = findDValues(xValues, cValues)
+
+    return aValues, bValues, cValues, dValues
+
+def findAVAlues(yValues):
+    AValues = []
+    for i in range(len(yValues) - 1):
+        AValues.append(A(yValues[i]))
+    return AValues
+
+def A(x):
+    return x
+
+def findCValues(xValues, yValues, start, end):
+    sizeX = len(xValues)
+
+    cValues = [0] * (sizeX - 1)
+    cValues[0] = start
+    cValues[1] = end
+    if start == 0 and end == 0:
+        ksiValues = [0, 0]
+        tetaValues = [0, 0]
+    elif start == 0:
+        ksiValues = [0, end / 2]
+        tetaValues = [0, end / 2]
+    else:
+        ksiValues = [start / 2, end / 2]
+        tetaValues = [start / 2, end / 2]
+
+    for i in range(2, sizeX):
+        h2 = xValues[i] - xValues[i - 1]  # hi
+        h1 = xValues[i - 1] - xValues[i - 2]  # hi-1
+
+        fiCur = fi(yValues[i - 2], yValues[i - 1], yValues[i], h1, h2)
+        ksiCur = ksi(ksiValues[i - 1], h1, h2)
+        tetaCur = teta(fiCur, tetaValues[i - 1], ksiValues[i - 1], h1, h2)
+
+        ksiValues.append(ksiCur)
+        tetaValues.append(tetaCur)
+
+    cValues[-1] = tetaValues[-1]
+
+    for i in range(sizeX - 2, 0, -1):
+        cValues[i - 1] = C(cValues[i], ksiValues[i], tetaValues[i])
+
+    return cValues
+
+# y1 = y_i-1
+# y2 = y_i
+# hi
+# c1 = c_i+1
+# c2 = c_i
+
+def C(c_i, ksi_i, teta_i):
+    return ksi_i * c_i + teta_i
+
+# y1 = yi-2
+# y2 = yi-1
+# y3 = yi
+# h1 = hi-1
+# h2 = hi
+def fi(y1, y2, y3, h1, h2):
+    return 3 * ((y3 - y2) / h2 - (y2 - y1) / h1)
+
+
+# функция ksi - расcчтывает значение ksi i-го элемента
+# ksi_i+1 = -h_i / (h_i-1  * ksi_i-1 * (h_i - h_i-1))
+# ksi_i = -h_i-1 / (h_i  * ksi_i + 2 * (h_i-1 - h_i))
+# ksi1 = ksi_i-1
+# h1 = h_i-1
+# h2 = h_i
+def ksi(ksi1, h1, h2):
+    return - h1 / (h2 * ksi1 + 2 * (h2 + h1))
+
+
+# fi - значение из функции fi()
+# teta - teta_i-1
+# ksi - ksi_i-1
+# h1 - h_i-1
+# h2 - h_i
+def teta(fi, teta_i, ksi_i, h1, h2):
+    return (fi - h1 * teta_i) / (h1 * ksi_i + 2 * (h2 + h1))
+
+def findBValues(xValues, yValues, cValues):
+    pass
+
+def B()
